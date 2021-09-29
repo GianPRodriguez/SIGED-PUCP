c231fe5fd3f15efb235ed4045ef9b4561a9304f8
Daniel Marquina Inca <70769202+1011Daniel@users.noreply.github.com>
2021-09-29 18:15:40 -0500

    Delete src directory

--- src/Variables.js
+++
@@ -1,4 +0,0 @@
-export const variables={
-   API_URL:"http://prototipoenv.eba-tdhde96g.us-west-2.elasticbeanstalk.com/",
-   PHOTO_URL:"http://prototipoenv.eba-tdhde96g.us-west-2.elasticbeanstalk.com/Photos/"
-}
