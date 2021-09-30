a4b5f841021a609de4088466efa6063244d527b9
paolo889patio <71732350+paolo889patio@users.noreply.github.com>
2021-09-30 00:39:15 -0500

    http
    http

--- src/Variables.js
+++ src/Variables.js
@@ -1,4 +1,4 @@
 export const variables={
-   API_URL:"https://prototipoenv.eba-tdhde96g.us-west-2.elasticbeanstalk.com/",
-   PHOTO_URL:"https://prototipoenv.eba-tdhde96g.us-west-2.elasticbeanstalk.com/Photos/"
+   API_URL:"http://prototipoenv.eba-tdhde96g.us-west-2.elasticbeanstalk.com/",
+   PHOTO_URL:"http://prototipoenv.eba-tdhde96g.us-west-2.elasticbeanstalk.com/Photos/"
 }
